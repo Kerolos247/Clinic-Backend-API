83d6a54fc09df57cf5e3d92a201c6eb1031adf5f
Kerolos247 <kerolos.adel754@gmail.com>
2025-10-17 18:51:35 +0300

    Update validation messages in DepartmentRequestDto

--- WebApplication1/Dto/DepartmentRequestDto.cs
+++ WebApplication1/Dto/DepartmentRequestDto.cs
@@ -4,8 +4,8 @@ namespace WebApplication1.Dto
 {
     public class DepartmentRequestDto
     {
-        [Required(ErrorMessage = "اسم القسم مطلوب")]
-        [StringLength(100, MinimumLength = 3, ErrorMessage = "اسم القسم يجب أن يكون بين 3 و 100 حرف")]
+        [Required(ErrorMessage = "Department name is required")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Section name must be between 3 and 100 characters.")]
         public string NameDepart { get; set; }
     }
 }
